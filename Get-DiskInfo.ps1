233574eb7e4a3ff1d061f77ac4c9e168ac22e246
andydeangelis <andy.deangelis@live.com>
2019-04-29 10:28:56 -0400

    v1.0

---
+++ Get-DiskInfo.ps1
@@ -0,0 +1,218 @@
+﻿<#
+	.SYNOPSIS
+		Small script to query all computers in the specified domain/OU that returns disk info into a sortable Excel report.
+	
+	.DESCRIPTION
+		Small script to query all computers in the specified domain/OU that returns disk info into a sortable Excel report.
+	
+	.PARAMETER SearchBase
+		Used by the Get-ADComputer function to return a list of servers.
+	
+	.PARAMETER Credential
+		Takes a PS Credential object to for remote authorization.
+	
+	.PARAMETER NumJobs
+		Determines number of parallel jobs to run.
+	
+	.NOTES
+		===========================================================================
+		Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2018 v5.5.150
+		Created on:   	4/25/2019 10:04 AM
+		Created by:   	andy-user
+		Organization:
+		Filename:
+		===========================================================================
+#>
+param
+(
+	[Parameter(Mandatory = $true)]
+	[string]$SearchBase,
+	[Parameter(Mandatory = $true)]
+	[System.Management.Automation.Credential()]
+	[ValidateNotNull()]
+	[System.Management.Automation.PSCredential]$Credential = [System.Management.Automation.PSCredential]::Empty,
+	[Parameter(Mandatory = $true)]
+	[ValidateRange(1, 256)]
+	[int]$NumJobs
+)
+
+# Source the function files.
+
+. ".\Test-TCPport.ps1"
+
+# First, let's make sure there are no stale jobs.
+
+Get-Job | Stop-Job
+Get-Job | Remove-Job
+
+if (-not (Get-InstalledModule -Name "ActiveDirectory" -ErrorAction SilentlyContinue))
+{
+	$osVersion = Get-CimInstance -ClassName Win32_OperatingSystem
+	
+	if ($osVersion.Caption -like "*Server*")
+	{
+		$osVersion.Caption
+		try
+		{
+			Set-ExecutionPolicy Unrestricted -Force
+			Write-Host "Installing AD PoSH Module modules on" $ENV:COMPUTERNAME -ForegroundColor Yellow
+			Add-WindowsFeature RSAT-AD-PowerShell
+			Import-Module ActiveDirectory
+		}
+		catch
+		{
+			
+			Write-Host "Unable to install or import the Active Directory PowerShell module. Please install manually or resolve connectivity issues." -ForegroundColor Red
+			break
+		}
+	}
+	else
+	{
+		Write-Host "This is not a server operating system. Please install the RSAT tools for $($osVersion.Caption) and run the script again."
+	}
+}
+else
+{
+	Import-Module ActiveDirectory
+}
+# Ensure the PSWindowsUpdate, PendingReboot and ImportExcel modules are installed on the local machine.
+
+if (-not (Get-InstalledModule -Name "ImportExcel" -ErrorAction SilentlyContinue))
+{
+	try
+	{
+		Set-ExecutionPolicy Unrestricted -Force
+		Write-Host "Installing modules on" $ENV:COMPUTERNAME -ForegroundColor Yellow
+		Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+		Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+		Install-Module -Name "ImportExcel" -Force -Scope AllUsers
+		Install-Module -Name "PSParallel" -Force -Scope AllUsers
+		Write-Host "All required PoSH modules successfully installed on" $ENV:COMPUTERNAME -ForegroundColor Green
+		Import-Module "ImportExcel"
+		Import-Module "PSParallel"
+	}
+	catch
+	{
+		
+		Write-Host "Unable to install one or more modules on" $ENV:COMPUTERNAME ". Please install manually or resolve connectivity issues." -ForegroundColor Red
+	}
+}
+else
+{
+	Write-Host "All required modules are already installed. Checking for module updates on" $ENV:COMPUTERNAME -ForegroundColor Green
+	try
+	{
+		Set-ExecutionPolicy Unrestricted -Force
+		Update-Module -Name "ImportExcel" -Force -Confirm:$false -ErrorAction Stop
+		Update-Module "PSParallel" -Force -Confirm:$false -ErrorAction Stop
+		Import-Module "ImportExcel"
+		Import-Module "PSParallel"
+		Write-Host "All required modules are up to date on" $ENV:COMPUTERNAME -ForegroundColor Green
+	}
+	catch
+	{
+		Write-Host "Unable to update one or more modules on" $ENV:COMPUTERNAME ". Please install manually or resolve connectivity issues." -ForegroundColor Red
+	}
+}
+
+$remoteComputers = Get-ADComputer -Credential $Credential -Filter { servicePrincipalName -notlike "*MSClusterVirtualServer*" } `
+								  -SearchBase "$SearchBase" #| ?{ $_.Enabled -eq "True" }
+
+# Get the date and time for the log file.
+
+$datetime = get-date -f MM-dd-yyyy_hh.mm.ss
+
+# Define the log file.
+
+$diskDrivesXLSX = "$PSScriptRoot\DiskDriveList_$datetime.xlsx"
+
+$failedComputerMessage = @()
+
+foreach ($computer in $remoteComputers)
+{
+	# Use the custom Test-TCPport function to verify we can connect to the computer.
+	
+	$tcpConnect = Test-TCPport -ComputerName $computer.Name -TCPport "5985"
+	$tcpConnectSec = Test-TCPport -ComputerName $computer.Name -TCPport "5986"
+	
+	if ($computer.Enabled -and ($tcpConnect -or $tcpConnectSec))
+	{
+		while (@(Get-Job | ?{ $_.State -eq "Running" }).Count -ge $NumJobs)
+		{
+			Write-Host "Waiting for open thread...($NumJobs Maximum)"
+			Start-Sleep -Seconds 3
+		}
+		
+		try
+		{
+			# For each computer, create a session to the remote computer, create the .ps1 files needed,
+			# then create the scheduled task.
+			
+			$session = New-PSSession -ComputerName $computer.Name -Credential $Credential -ErrorAction Stop
+			Invoke-Command -Session $session -AsJob -JobName $computer.Name -ErrorAction Stop -ScriptBlock {
+				
+				Get-PSDrive -PSProvider FileSystem | Select-Object PSComputerName, Name, Root, Description, @{ Name = "Used"; Expression = { $_.Used/1GB } }, @{ Name = "Free"; Expression = { $_.Free/1GB } }
+			}
+			
+			$connectedComputers += $computer.Name
+			
+		}
+		catch
+		{
+			"Unable to retrieve drive information for device $($computer.Name)."
+			$failedComputerMessage += "Connection succeeded to $($computer.Name), but unable to retrieve drive information."
+		}
+	}
+	else
+	{
+		Write-Host "Unable to connect to $($computer.Name)." -ForegroundColor Red
+		$failedComputerMessage += "Unable to connect to $($computer.Name) to to retrieve drive information."
+	}
+	
+}
+
+if ($failedComputerMessage) { $failedComputerMessage | Out-File "$PSScriptRoot\FailedInstalledUpdates_$datetime.log" -Append }
+
+$jobs = (Get-Job)
+
+Write-Host "Waiting for outstanding jobs..." -NoNewline -ForegroundColor DarkGreen
+do
+{
+	Write-Host "." -NoNewline -ForegroundColor DarkGreen
+	Start-Sleep -Milliseconds 500
+}
+while ((Get-Job -State Running).Count -gt 0)
+
+Write-Host "." -ForegroundColor DarkGreen
+Write-Host "All jobs completed!" -ForegroundColor Magenta
+
+$driveStatus = @()
+
+foreach ($job in $jobs)
+{
+	$data = Get-Job -Name $job.Name | Receive-Job
+	Remove-Job $job
+	
+	
+	#$data = $data | ?{ $_.HotFixID -ne $null } | select PSComputerName, HotFixID, InstalledBy, InstalledOn, Description, Caption
+	
+	$driveStatus += $data
+	
+	Clear-Variable data
+}
+
+Get-PSSession | Remove-PSSession
+
+
+try
+{
+	$Worksheet = "Worksheet"
+	$Table = "Table"
+	
+	$excel = $driveStatus | Export-Excel -Path $diskDrivesXLSX -AutoSize -WorksheetName $Worksheet -FreezeTopRow -TableName $Table -PassThru
+	$excel.Save(); $excel.Dispose()
+}
+catch
+{
+	"Unable to create spreadsheet."
+}
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # GetDiskInfo
+
+Description:
+
+	- This script is an easy to use method to query remote machines for disk space utilization based on Active Directory OU membership.
+	
+Parameters:
+
+	- The main wrapper file is called Get-DiskInfo.ps1. It is the main file that multi-threads the update process and generates the Excel reports. It takes the following parameters:
+		-SearchBase: Mandatory string parameter. This is the distinguished name of the OU in Active Directory housing the target machines.
+		-Credential: Mandatory PSCredential object. The PSCredential object used to run the jobs. Must be an admin on both the controller server and target machines.
+		-NumJobs: Optional positive integer value. Optional parameter to set number of simultaneous jobs. If not set, the script will determine the number of simultaneous jobs based on the number of logical cores.
+
+Pre-reqs:
+
+	- This has only been tested with PoSH 4 and higher. PoSH v3.0 "should" work, but I haven't tested it. With that said, don't expect it to run on Server 2003/2008/XP/etc.
+	- The controller server/workstation running this script should have internet access, specifically to the Microsoft PSGallery. If internet access from this machine exists, the appropriate modules will be installed automatically.
+		- If internet access is not possible from the controller node, you will need to manually install the ImportExcel module from the PSGallery.
+	- PS Remoting ports need to be open from the controller server/workstation to all target servers in order to pass the Invoke-Command cmdlet.
+	- If running from the main launcher script, the account specified in the PSCredential object must have the ability to read from AD (no local accounts).
+	- The PSCredential object passed to the main script must have admin rights on the target servers/workstations.
+	- The script ignores Disabled computer accounts, so it doesn't try to connect to them.
+	- The script does test connectivity to computer accounts, so if a computer account is enabled but not responding (i.e. powered off), it will also be ignored.
+	
+Files:
+
+	- The file structure must remain as is in order to run the script. 
+	- Test-TCPPort.ps1 tests a TCP socket connect to a specified TCP port. While not as robust as the built-in Test-NetConnection function, it returns much faster if the connection fails (important for scripts). It uses the System.Net.Sockets.TcpClient class to create the connection.
+			Example 1:
+			
+				PS C:\Scripts\Projects\WindowsUpdate> Test-TCPport -ComputerName andy-2k16-vmm2 -TCPport 5985
+
+				hostname         port open
+				--------         ---- ----
+				{andy-2k16-vmm2} 5985 True
+				
+Main Script Usage
+
+	- Usage of the script is pretty easy. Simply pass the DN, the credential object and the number of simultaneous jobs to use.
+		Example 1:
+		
+			PS> .\Get-DiskInfo.ps1 -SearchBase "ou=servers,dc=domain,dc=com" -Credential (Get-Credential) -NumJobs 16
+				- The above command will query the disk drives on all enabled computer accounts in the Servers OU (maximum 16 jobs at a time), and it will export the list of available updates into an Excel spreadsheet.
